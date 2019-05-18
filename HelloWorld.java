15b8e104a6c8e8d38b026ee4eecff6b248d19207
newrkq <48463097+nreqzit@users.noreply.github.com>
2019-05-18 16:38:09 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-	public static void main(String[]args){
+public class HelloWorld {
+	public static void main (String[]args) {
 		// Print Hello World
 		System.out.println("Hello World");
 }
